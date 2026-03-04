a84aebf995559a3966d2f7cd5b68536a5a90cb36
Daniel <daniel10.cd75@gmail.com>
2026-03-04 20:09:51 -0300

    fix

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   const [display, setDisplay] = useState<string>('0');
